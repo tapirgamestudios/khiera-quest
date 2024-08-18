47e84f5707cdd3d2812ea152fa36e030f2867049
Gwilym Inzani <email@gwilym.dev>
2024-08-18 22:36:43 +0100

    Add transparent colour

--- map/planets.tsx
+++ map/planets.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="planets" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
- <image source="../built-to-scale/gfx/background.png" width="256" height="256"/>
+ <image source="../built-to-scale/gfx/background.png" trans="000000" width="256" height="256"/>
 </tileset>
