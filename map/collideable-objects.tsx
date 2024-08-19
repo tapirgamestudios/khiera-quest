5a3355b3d66310d25951f102a1cca7b3d72ad97f
Gwilym Inzani <email@gwilym.dev>
2024-08-19 20:07:59 +0100

    Draft the tmx for moving objects

---
+++ map/collideable-objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="collideable-objects" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../built-to-scale/gfx/collideable-objects.png" trans="000000" width="32" height="32"/>
+</tileset>
