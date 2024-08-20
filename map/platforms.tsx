1dcdfd43dd49378f9fe8f36030f3d92e78fbe9d5
Gwilym Inzani <email@gwilym.dev>
2024-08-20 15:31:58 +0100

    Add some texture to the floor

--- map/platforms.tsx
+++ map/platforms.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="platforms" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
  <image source="../built-to-scale/gfx/platforms.png" trans="000000" width="256" height="256"/>
+ <tile id="102" probability="20"/>
 </tileset>
