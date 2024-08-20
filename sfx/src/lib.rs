1d231346a3cc192c9691a0c3f74e4ed8cac917da
Gwilym Inzani <email@gwilym.dev>
2024-08-20 15:20:27 +0100

    Use the new space intro music

--- sfx/src/lib.rs
+++ sfx/src/lib.rs
@@ -3,4 +3,4 @@
 extern crate alloc;
 
 pub static GROUND_MUSIC: agb_tracker::Track = agb_tracker::include_xm!("ground.xm");
-pub static SPACE_MUSIC: agb_tracker::Track = agb_tracker::include_xm!("enacostione_-_spacecraft.xm");
+pub static SPACE_MUSIC: agb_tracker::Track = agb_tracker::include_xm!("space.xm");
